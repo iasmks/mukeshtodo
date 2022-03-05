509a78ab975797e49f4a1af6c48185bdb458ec15
MUKESH KUMAR SAH <82627741+iasmks@users.noreply.github.com>
2022-03-05 16:42:19 +0530

    Add files via upload

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Footer = () => { 
+    return (
+        <footer className="bg-info text-white py-3 fixed-bottom">
+            <p className="text-center">
+            Copyright &copy; All rights reserved, 2022
+            </p>
+        </footer>
+    )
+}
